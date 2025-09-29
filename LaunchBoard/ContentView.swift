170533e0aad356b29e610f659ec502d8394e289e
Fan Bin <fanbin0602@163.com>
2025-09-29 13:22:19 +0800

    Initial Commit

---
+++ LaunchBoard/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  LaunchBoard
+//
+//  Created by 范斌 on 2025/9/29.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ LaunchBoard/LaunchBoardApp.swift
@@ -0,0 +1,17 @@
+//
+//  LaunchBoardApp.swift
+//  LaunchBoard
+//
+//  Created by 范斌 on 2025/9/29.
+//
+
+import SwiftUI
+
+@main
+struct LaunchBoardApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
